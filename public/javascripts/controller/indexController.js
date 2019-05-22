7e2442b00372572cba937efd059171c24a3b7c71
Tolga <caglartolgakaya@hotmail.com>
2019-05-22 23:25:34 +0300

    user Joined

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,15 +1,37 @@
 app.controller('indexController', ['$scope','indexFactory', ($scope, indexFactory) =>{
 
-    const connectionOptions = {
-        reconnectionAttempts: 3,
-        reconnectionDelay: 600
+    $scope.messages = [];
+
+    $scope.init = () =>{
+      const username = prompt('Please Enter Username');
+
+        if(username)
+            initSocket(username);
+        else
+            return false;
     };
 
-    indexFactory.connectSocket('http://localhost:3000',{
-    }).then((socket)=>{
-        console.log('Connected ', socket);
-    }).catch((err)=>{
-        console.log(err)
-    });
+    function initSocket(username){
+        const connectionOptions = {
+            reconnectionAttempts: 3,
+            reconnectionDelay: 600
+        };
+
+        indexFactory.connectSocket('http://localhost:3000',connectionOptions)
+         .then((socket)=>{
+            socket.emit('newUser', {username});
+            socket.on('newUser', (data)=>{
+                const messageData = {
+                    type:0, //info
+                    username: data.username
+                };
+
+                $scope.messages.push(messageData);
+                $scope.$apply();
+            });
+        }).catch((err)=>{
+            console.log(err)
+        });
+    };
 
 }]);
