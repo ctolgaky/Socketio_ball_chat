59aaca3921cd52005446a2553e808b1f992bb971
Tolga <caglartolgakaya@hotmail.com>
2019-05-23 23:35:25 +0300

    ball position

--- public/javascripts/controller/indexController.js
+++ public/javascripts/controller/indexController.js
@@ -1,6 +1,7 @@
 app.controller('indexController', ['$scope','indexFactory', ($scope, indexFactory) =>{
 
-    $scope.messages = [];
+    $scope.messages = [ ];
+    $scope.players = { };
 
     $scope.init = () =>{
       const username = prompt('Please Enter Username');
@@ -20,15 +21,49 @@ app.controller('indexController', ['$scope','indexFactory', ($scope, indexFactor
         indexFactory.connectSocket('http://localhost:3000',connectionOptions)
          .then((socket)=>{
             socket.emit('newUser', {username});
+
+            socket.on('initPlayers', (players)=>{
+                $scope.players = players;
+                $scope.$apply();
+            })
+
             socket.on('newUser', (data)=>{
                 const messageData = {
-                    type:0, //info
+                    type:{
+                        code: 0,    //user or server message
+                        message: 1  // login or disconnect message
+                    }, //info
                     username: data.username
+
                 };
 
                 $scope.messages.push(messageData);
+                $scope.players[data.id] = data;
                 $scope.$apply();
             });
+            socket.on('disUser', (data)=>{
+                const messageData = {
+                    type:{
+                        code: 0,
+                        message: 0
+                    }, //info
+                    username: data.username
+                };
+                $scope.messages.push(messageData);
+                delete $scope.players[data.id];
+                $scope.$apply();
+            });
+            let animate = false;
+            $scope.onClickPlayer = ($event)=>{
+              console.log($event.offsetX, $event.offsetY);
+                if(!animate){
+                    animate= true;
+                    $('#'+ socket.id).animate({'left':$event.offsetX, 'top':$event.offsetY}, ()=>{
+                        animate= false;
+                    });
+                }
+
+            };
         }).catch((err)=>{
             console.log(err)
         });
--- src/socket_api.js
+++ src/socket_api.js
@@ -4,7 +4,7 @@ const io = socketio();
 const socketApi = { };
 socketApi.io = io;
 
-const users = [ ];
+const users = { };
 
 io.on('connection', (socket) =>{
 
@@ -19,10 +19,18 @@ io.on('connection', (socket) =>{
            }
        };
        const userData = Object.assign(data, defaultData);
-       users.push(userData);
+       users[socket.id] = userData;
 
-       socket.broadcast.emit('newUser', userData);
-    })
+       socket.broadcast.emit('newUser', users[socket.id]);
+       socket.emit('initPlayers', users)
+    });
+
+    socket.on('disconnect', ()=>{
+
+        socket.broadcast.emit('disUser', users[socket.id]);
+        delete users[socket.id];
+
+    });
 
 });
 
