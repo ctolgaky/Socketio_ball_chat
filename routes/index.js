332c37e7b7803d40169fb565da97f147763c76ae
Tolga <caglartolgakaya@hotmail.com>
2019-06-07 00:06:26 +0300

    configFactory

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Balls Chat' });
 });
 
+router.get('/getEnv', function(req, res, next) {
+  const envData =  require('../config/env.json')[process.env.NODE_ENV || 'development']; //ortam değişkeni
+  res.json(envData);
+});
+
 module.exports = router;
